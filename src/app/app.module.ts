e334d8c249921384c97f074824f7d1d73cd76210
ThanneruPrasanth <tprasanth.tech.t@gmail.com>
2022-09-02 16:30:08 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store/src';
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoReducer } from './reducers/todo-reducer';
